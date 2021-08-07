0681cf9907d2ee196a165d9db860fa7995b3342a
Amine Grassi <grassi.amine@hotmail.fr>
2021-08-07 14:14:29 +0100

    dataBase Connection + create Crud  User

--- index.js
+++ index.js
@@ -2,34 +2,45 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const port = 3000
+// database connection 
+require('./database/connect')
+// reuire model
+const User = require('./models/userSchema')
 
 //Morgan Config
 app.use(morgan('dev'))
+// config body parser
+app.use(express.json())
 
 app.get('/', (req, res) => {
-    res.json({message : 'Hello World! Grassi'})
+    res.json({ message: 'Hello World! Grassi' })
 })
 
 // get all user
-app.get('/users', (req, res) => {
-    res.json({ message: 'get all users' });
+app.get('/users', async (req, res) => {
+    const users = await User.find({});
+    res.json(users)
 })
 // get one user by id
-app.get('/users/:id', (req, res) => {
-    res.json({ message: 'get one user by id' });
+app.get('/users/:id', async (req, res) => {
+    const user = await User.findById(req.params.id)
+    res.json(user);
 })
 // add one user
-app.post('/users', (req, res) => {
-    res.json({ message: 'user added successfully' });
+app.post('/users', async(req, res) => {
+    const createdUser = await User.create(req.body)
+    res.json(createdUser);
 })
 // update user by id
-app.put('/users/:id', (req, res) => {
-    res.json({ message: 'user updated' });
+app.put('/users/:id', async(req, res) => {
+    const updatedUser = await User.findByIdAndUpdate(req.params.id,req.body)
+    res.json(updatedUser);
 })
 
 // delete user by id
-app.delete('/users/:id', (req, res) => {
-    res.json({ message: 'deleted user' });
+app.delete('/users/:id', async(req, res) => {
+    const deletedUser = await User.findByIdAndDelete(req.params.id)
+    res.json({ message: 'deleted user successfully' });
 })
 
 app.listen(port, () => {
---
+++ models/userSchema.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    firstName: { type: String, required: true },
+    lastName: String,
+    age: Number,
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
