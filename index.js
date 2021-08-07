59bfe63cc2710f5d077490e973a48894f1b7727d
Amine Grassi <grassi.amine@hotmail.fr>
2021-08-07 12:52:12 +0100

    add API root

--- index.js
+++ index.js
@@ -7,15 +7,31 @@ const port = 3000
 app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Grassi')
+    res.json({message : 'Hello World! Grassi'})
 })
 
 // get all user
+app.get('/users', (req, res) => {
+    res.json({ message: 'get all users' });
+})
 // get one user by id
+app.get('/users/:id', (req, res) => {
+    res.json({ message: 'get one user by id' });
+})
 // add one user
+app.post('/users', (req, res) => {
+    res.json({ message: 'user added successfully' });
+})
 // update user by id
+app.put('/users/:id', (req, res) => {
+    res.json({ message: 'user updated' });
+})
+
 // delete user by id
+app.delete('/users/:id', (req, res) => {
+    res.json({ message: 'deleted user' });
+})
 
 app.listen(port, () => {
-  console.log(`Application listening at http://localhost:${port}`)
+    console.log(`Application listening at http://localhost:${port}`)
 })
