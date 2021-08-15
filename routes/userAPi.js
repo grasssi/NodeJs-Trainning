a33373b77a9377b20d175ec7bdd8ec147bc5abfc
Amine Grassi <grassi.amine@hotmail.fr>
2021-08-15 21:20:28 +0100

    try adn catch

--- routes/userAPi.js
+++ routes/userAPi.js
@@ -16,24 +16,48 @@ router.get('/users', async (req, res) => {
 })
 // get one user by id
 router.get('/users/:id', async (req, res) => {
+    try {
     const user = await User.findById(req.params.id)
     res.json(user);
+}
+catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal server error' });
+}
 })
 // add one user
 router.post('/users', async (req, res) => {
+    try {
     const createdUser = await User.create(req.body)
     res.json(createdUser);
+}
+catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal server error' });
+}
 })
 // update user by id
 router.put('/users/:id', async (req, res) => {
+      try {
     const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true })
     res.json(updatedUser);
+}
+catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal server error' });
+}
 })
 
 // delete user by id
 router.delete('/users/:id', async (req, res) => {
+    try {
     const deletedUser = await User.findByIdAndDelete(req.params.id)
     res.json({ message: 'deleted user successfully' });
+}
+catch (err) {
+    console.log(err);
+    res.status(500).json({ message: 'Internal server error' });
+}
 })
 
 // affect todo to user
